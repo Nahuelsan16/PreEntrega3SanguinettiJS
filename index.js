da385fc87c006aafcfa0a335d300431e372f418a
Nahuel Sanguinetti <nahuelsanguinetti16@gmail.com>
2023-08-15 22:47:32 -0300

    cree el repositorio

---
+++ index.js
@@ -0,0 +1,9 @@
+alert('Bienvenido a pinturas Nahuel')
+
+let respuesta = prompt('desea pintar su hogar?') 
+while (respuesta === 'si') {
+   respuesta = prompt('cuantos metros desea pintar?')
+    
+}
+
+alert('gracias')
