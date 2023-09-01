9dd3be12acf7e8f6225776f7656613e74f3440c2
Nahuel Sanguinetti <nahuelsanguinetti16@gmail.com>
2023-08-31 23:05:44 -0300

    modoficaciones en js

--- index.js
+++ index.js
@@ -2,75 +2,75 @@ alert('Bienvenido a pinturas Nahuel!');
 
 let respuesta = parseInt(prompt("¡Ingresa un n° del 1 al 10 para ganar un descuento!"));
 if (respuesta === 7) {
-   alert("Felicidades! Ganó un %25 de descuento en su compra! Codigo Pin25")
+   alert("Felicidades! Ganó un %25 de descuento en su compra! Codigo 25")
 } else {
    alert("Pruebe suerte de nuevo mañana ;)")
 }
 
-// ------------------------------------------------------------------
-// let respuesta = prompt('desea un presupuesto de pintura ya?')
-// if (respuesta === 'si') {}
-// else {
-//    alert('disfrute nuestra web ;)');
+// // ------------------------------------------------------------------
+// // let respuesta = prompt('desea un presupuesto de pintura ya?')
+// // if (respuesta === 'si') {}
+// // else {
+// //    alert('disfrute nuestra web ;)');
+// // }
+
+// // // ------------------------------------
+// // function PresupuestoPorMetro(MetrosCuadrados, PrecioPorMetro) {
+// //    const Presupuesto = MetrosCuadrados * PrecioPorMetro
+// //    return Presupuesto
+// //  }
+// // //   ----------------------------------------
+// // const PrecioPorMetro = 350
+// // const MetrosCuadrados = parseFloat(prompt('ingrese cantidad en m² que desea pintar'))
+
+// // const resultado = PresupuestoPorMetro(MetrosCuadrados, PrecioPorMetro)
+// // alert(resultado)
+
+
+// //----------------------------------------------------------
+
+
+// class Producto {
+//    constructor(tipo, precio, litros) {
+//       this.tipo = tipo
+//       this.precio = precio
+//       this.litros = litros
+//    }
+//    sumaIva() {
+//       this.precio = this.precio * 1.21;
+//    }
+// }
+// // ---Productos--
+// const prod1 = new Producto('Pintura al Agua', 700, '4L')
+// const prod2 = new Producto('Pintura al Agua', 1000, '10L')
+// const prod3 = new Producto('Pintura al Agua', 2000, '20L')
+// const prod4 = new Producto('Membrana Liquida', 1500, '10L')
+// const prod5 = new Producto('Membrana Liquida', 3200, '20L')
+
+// console.log(prod1);
+// console.log(prod2);
+// console.log(prod3);
+// console.log(prod4);
+// console.log(prod5);
+// prod1.sumaIva();
+// prod2.sumaIva();
+// prod3.sumaIva();
+// prod4.sumaIva();
+// prod5.sumaIva();
+
+
+// // --------------------------------------------------------------
+// const wishList = []
+// let productosQueBusco = 4
+
+// for (let i = 0; i < productosQueBusco; i++) {
+//    const entrada = prompt("Ingrese cada pintrua que este buscando")
+//    wishList.push(entrada.toUpperCase())
+   
 // }
 
-// // ------------------------------------
-// function PresupuestoPorMetro(MetrosCuadrados, PrecioPorMetro) {
-//    const Presupuesto = MetrosCuadrados * PrecioPorMetro
-//    return Presupuesto
-//  }
-// //   ----------------------------------------
-// const PrecioPorMetro = 350
-// const MetrosCuadrados = parseFloat(prompt('ingrese cantidad en m² que desea pintar'))
-
-// const resultado = PresupuestoPorMetro(MetrosCuadrados, PrecioPorMetro)
-// alert(resultado)
-
-
-//----------------------------------------------------------
-
-
-class Producto {
-   constructor(tipo, precio, litros) {
-      this.tipo = tipo
-      this.precio = precio
-      this.litros = litros
-   }
-   sumaIva() {
-      this.precio = this.precio * 1.21;
-   }
-}
-// ---Productos--
-const prod1 = new Producto('Pintura al Agua', 700, '4L')
-const prod2 = new Producto('Pintura al Agua', 1000, '10L')
-const prod3 = new Producto('Pintura al Agua', 2000, '20L')
-const prod4 = new Producto('Membrana Liquida', 1500, '10L')
-const prod5 = new Producto('Membrana Liquida', 3200, '20L')
-
-console.log(prod1);
-console.log(prod2);
-console.log(prod3);
-console.log(prod4);
-console.log(prod5);
-prod1.sumaIva();
-prod2.sumaIva();
-prod3.sumaIva();
-prod4.sumaIva();
-prod5.sumaIva();
-
-
-// --------------------------------------------------------------
-const wishList = []
-let productosQueBusco = 4
-
-for (let i = 0; i < productosQueBusco; i++) {
-   const entrada = prompt("Ingrese cada pintrua que este buscando")
-   wishList.push(entrada.toUpperCase())
-   
-}
-
-console.log(wishList)
-alert(wishList.join("\n"))
+// console.log(wishList)
+// alert(wishList.join("\n"))
 
 // ----------------------------------------------------------------------
 
@@ -80,14 +80,14 @@ function aplicarDescuento(montoInicial, porcentajeDescuento) {
    return montoFinal
 }
 
-const codigoDescuento = prompt('Inserte pin de descuento')
-if (respuesta === 'Pin25') {
+let codigoDescuento = parseInt(prompt('Tiene el pin de descuento?')) 
+if (codigo === 25) {
    const montoInicial = parseFloat(prompt('ingrese el monto inicial'))
    const porcentajeDescuento = 25
-   const resultado = aplicarDescuento(montoInicial, porcentajeDescuento)
-   alert(resultado)
-} else {
-   alert("la respuesta " + respuesta + " es pin no valido, lo sentimos" )
+   const descuentoAplicado = aplicarDescuento(montoInicial, porcentajeDescuento)
+   alert("el total es $" + descuentoAplicado)
+} else (codigo != 25); {
+   alert("la respuesta " + codigo + " no es el pin valido, lo sentimos" )
 }
 
 alert('gracias, disfuten nuestra web')
